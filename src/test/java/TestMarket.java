9f802cfccf28a6f79704bf339eea2f018d26a0f6
Малчинский Владимир <malchinskiy@pup.local>
2017-05-16 17:55:51 +0300

    first commit

---
+++ src/test/java/TestMarket.java
@@ -0,0 +1,27 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.testng.annotations.Test;
+
+import javax.swing.text.Element;
+
+import static com.codeborne.selenide.Condition.disappear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+/**
+ * Created by malchinskiy on 16.05.2017.
+ */
+public class TestMarket {
+    @Test
+    public void logIn() {
+        Configuration.browser = "chrome";
+        open("http://market.test2.local");
+        SelenideElement username = $("#username").shouldBe(visible).setValue("Notus");
+        SelenideElement password = $("#password").shouldNot(disappear).setValue("123qweqwe");
+        SelenideElement submitButton = $(".btn-link-i");
+    }
+
+}
